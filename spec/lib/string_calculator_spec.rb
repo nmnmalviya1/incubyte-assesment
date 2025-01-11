bf10f8f1ed0e8f5c2fa56c853303d8c9169ba0c3
Naman <nmnmalviya1@gmail.com>
2025-01-11 19:33:36 +0530

    Add tests for empty string and single number inputs

--- spec/lib/string_calculator_spec.rb
+++ spec/lib/string_calculator_spec.rb
@@ -4,4 +4,9 @@ describe 'StringCalculator' do
     it 'returns 0 for an empty string' do
         expect(StringCalculator.add("")).to eq(0)
     end
+    
+    it 'returns number when one number string provided' do
+        expect(StringCalculator.add("5")).to eq(5)
+    end
+    
 end
